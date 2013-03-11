ceylon/ceylon-compiler

commit 76e610663c750ed167c82b25ada0b67c5129b576

Issue #1026: Figure out what the RHS type is properly

src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
@@ -1332,7 +1332,7 @@ public class ExpressionTransformer extends AbstractTransformer {
 
     private JCExpression transformOverridableBinaryOperator(Tree.BinaryOperatorExpression op, Interface compoundType) {
         ProducedType leftType = getSupertype(op.getLeftTerm(), compoundType);
-        ProducedType rightType = getTypeArgument(leftType);
+        ProducedType rightType = getTypeArgument(getSupertype(op.getRightTerm(), compoundType));
         return transformOverridableBinaryOperator(op, leftType, rightType);
     }
 
@@ -1427,11 +1427,12 @@ public class ExpressionTransformer extends AbstractTransformer {
         }
         
         final ProducedType leftType = getSupertype(op.getLeftTerm(), compoundType);
-        final ProducedType rightType = getMostPreciseType(op.getLeftTerm(), getTypeArgument(leftType, 0));
+        final ProducedType resultType = getMostPreciseType(op.getLeftTerm(), getTypeArgument(leftType, 0));
+        final ProducedType rightType = getTypeArgument(getSupertype(op.getRightTerm(), compoundType));//getMostPreciseType(op.getLeftTerm(), getTypeArgument(leftType, 0));
 
         // we work on boxed types
         return transformAssignAndReturnOperation(op, op.getLeftTerm(), boxResult, 
-                leftType, rightType, 
+                leftType, resultType, 
                 new AssignAndReturnOperationFactory(){
             @Override
             public JCExpression getNewValue(JCExpression previousValue) {

commit 71ebb1bcedc903582b9ca25840ac6f23e8ab2659

Fix interop (+ on byte) broken by 76e6106

src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
@@ -1428,7 +1428,7 @@ public class ExpressionTransformer extends AbstractTransformer {
         
         final ProducedType leftType = getSupertype(op.getLeftTerm(), compoundType);
         final ProducedType resultType = getMostPreciseType(op.getLeftTerm(), getTypeArgument(leftType, 0));
-        final ProducedType rightType = getTypeArgument(getSupertype(op.getRightTerm(), compoundType));//getMostPreciseType(op.getLeftTerm(), getTypeArgument(leftType, 0));
+        final ProducedType rightType = getMostPreciseType(op.getLeftTerm(), getTypeArgument(getSupertype(op.getRightTerm(), compoundType)));
 
         // we work on boxed types
         return transformAssignAndReturnOperation(op, op.getLeftTerm(), boxResult, 