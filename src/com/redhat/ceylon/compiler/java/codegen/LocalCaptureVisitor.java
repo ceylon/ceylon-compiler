154f734e4941e57837d26b7bd4cff4fb06725f32
Tom Bentley <tom.bentley@cantab.net>
2014-01-28 13:16:49 +0000

    hoist local values: Rewrite LocalCaptureVisitor to handle fat arrow local values.

--- src/com/redhat/ceylon/compiler/java/codegen/LocalCaptureVisitor.java
+++ src/com/redhat/ceylon/compiler/java/codegen/LocalCaptureVisitor.java
@@ -1,6 +1,7 @@
 package com.redhat.ceylon.compiler.java.codegen;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.redhat.ceylon.compiler.typechecker.model.Class;
@@ -11,7 +12,6 @@ import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.compiler.typechecker.model.Value;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.Term;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
 /**
@@ -21,6 +21,71 @@ import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 public class LocalCaptureVisitor extends Visitor {
     
     public LocalCaptureVisitor() {
+    }
+    
+    private ArrayList<Scope> declarationScopes = new ArrayList<Scope>();
+    private void pushScope(Scope scope) {
+        declarationScopes.add(scope);
+    }
+    private Scope popScope() {
+        return declarationScopes.remove(declarationScopes.size()-1);
+    }
+    
+    public void visit(Tree.AnyClass that) {
+        pushScope(that.getDeclarationModel());
+        super.visit(that);
+        popScope();
+    }
+    
+    public void visit(Tree.AnyInterface that) {
+        pushScope(that.getDeclarationModel());
+        super.visit(that);
+        popScope();
+    }
+    
+    public void visit(Tree.AnyMethod that) {
+        pushScope(that.getDeclarationModel());
+        super.visit(that);
+        popScope();
+    }
+    
+    public void visit(Tree.AnyAttribute that) {
+        pushScope(that.getDeclarationModel());
+        super.visit(that);
+        popScope();
+    }
+    
+    public void visit(Tree.AttributeSetterDefinition that) {
+        pushScope(that.getDeclarationModel());
+        super.visit(that);
+        popScope();
+    }
+    
+    public Iterable<Scope> containingDeclarationScopes() {
+        return new Iterable<Scope>() {
+            @Override
+            public Iterator<Scope> iterator() {
+                return new Iterator<Scope>() {
+                    int index = declarationScopes.size()-1;
+                    
+                    @Override
+                    public boolean hasNext() {
+                        return index >= 0;
+                    }
+
+                    @Override
+                    public Scope next() {
+                        Scope result = declarationScopes.get(index);
+                        index--;
+                        return result;
+                    }
+                    @Override
+                    public void remove() {
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
+        };
     }
     
     public static boolean isStaticDeclaration(Scope d) {
@@ -54,14 +119,12 @@ public class LocalCaptureVisitor extends Visitor {
     public void visit(Tree.BaseMemberExpression that) {
         if (noErrors(that)
                 && !that.getDeclaration().isToplevel()) {
-            Scope useScope = that.getScope();
-            Scope scope = useScope;
             Scope declScope = that.getDeclaration().getScope();
             while (declScope != null 
                     && !(declScope instanceof Declaration)) {
                 declScope = declScope.getContainer();
             }
-            while (true) {
+            for (Scope scope : containingDeclarationScopes()) {
                 if (scope == null) {
                     break;
                 }
@@ -71,7 +134,6 @@ public class LocalCaptureVisitor extends Visitor {
                 if (isStaticDeclaration(scope) && !(scope.getContainer() instanceof Class)) {
                     addCapture((Declaration)scope, that.getDeclaration());
                 }
-                scope = scope.getContainer();
             }
         }
         
