e62bbd6051a7bc980423391531e740414d2c4a3f
Tom Bentley <tom.bentley@cantab.net>
2014-12-05 11:59:43 +0000

    Ensure we downcast to either Class or MemberClass when getting a classes constructor, otherwise the type literal end up being just a ConstructorModel, rather than a Constructor or MemberClassConstructor

--- src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
+++ src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
@@ -1341,7 +1341,8 @@ public class ExpressionTransformer extends AbstractTransformer {
         if(!expr.getWantsDeclaration()){
             if (expr.getDeclaration() instanceof Constructor) {
                 JCExpression classLiteral = makeTypeLiteralCall(expr.getType().getTypeModel().getQualifyingType(), false, expr.getTypeModel());
-                TypeDeclaration classModelDeclaration = (TypeDeclaration)typeFact().getLanguageModuleModelDeclaration("ClassModel");
+                TypeDeclaration classModelDeclaration = (TypeDeclaration)typeFact().getLanguageModuleModelDeclaration(
+                        expr.getType().getTypeModel().getQualifyingType().getDeclaration().isMember() ? "MemberClass" : "Class");
                 JCTypeCast typeCast = make().TypeCast(
                         makeJavaType(classModelDeclaration.getProducedType(null, 
                                 List.of(expr.getType().getTypeModel().getQualifyingType(), 
